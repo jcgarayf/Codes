54cb62d5cba560aeba03135c0c35bf635cee7486
jcgarayf <90216627+jcgarayf@users.noreply.github.com>
2021-11-29 21:14:38 -0600

    Update SRNormsPlot.m
    Comments updated

--- OSM_with_cross-points_MATLAB/SRNormsPlot.m
+++ OSM_with_cross-points_MATLAB/SRNormsPlot.m
@@ -1,13 +1,16 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%  Computation of optimal normalized boundary parameters and spectral radius of iteration operator%%
+%%  Plot of curve Spectral radius vs. Normalized alpha                                             %%  
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 clear
-%h=2^-12;
-%ng=2*h/(1+4*h);
-ng=0.05;
-nb=0;
-na=[linspace(0.01,0.1,10) linspace(0.1,1,40) linspace(1,8,60) 10 15 23 28 linspace(30,100,10)];
-%na=0.7;
-p=2;
-q=2;
-kmax=15;
+ng=0.05; %normalized gamma
+nb=0;% normalized beta (for Ventcel transmission conditions)
+na=[linspace(0.01,0.1,10) linspace(0.1,1,40) linspace(1,8,60) 10 15 23 28 linspace(30,100,10)]; % normalized alpha values
+
+p=2; %number of subdomains in horizontal direction 
+q=2; %number of subdomains in vertical direction
+kmax=15; % max number of generalized Fourier modes
 nna=length(na);
 SR=zeros(1,nna);
 tic
@@ -15,7 +18,7 @@ for i=1:nna
     %tic
     disp(i)
     try
-        SR(i)=SRNorms(na(i),nb,ng,p,q,kmax);    
+        SR(i)=SRNorms(na(i),nb,ng,p,q,kmax); % computes spectral radius of iteration operator (for the error series coefficients)  
     catch
         SR(i)=NaN;
         continue;
